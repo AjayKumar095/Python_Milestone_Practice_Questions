9c065b55a87516440c495496e5dc23eb0e1f395b
Ajay <jaykumar002kori@gmail.com>
2024-07-20 13:21:57 +0530

    commit

--- Modules_and_FileHandling/Question13/ecommerce/order/processing.py
+++ Modules_and_FileHandling/Question13/ecommerce/order/processing.py
@@ -1,7 +1,9 @@
 def place_order(cart, customer_info):
     # Function to place an order
+    ## here we can implements logics to place an order
     pass
 
 def process_payment(order_id, payment_info):
     # Function to process payment for an order
+    # here we can implements logics to process payments
     pass
